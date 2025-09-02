3dad81c1d565ab8da3109759d47dbac43a5ee30c
Abhishek Shukla <abhishekshukla12897@gmail.com>
2025-09-02 09:57:32 +0530

    change in index.js

--- backend/index.js
+++ backend/index.js
@@ -35,6 +35,6 @@ app.use('/api/rides',rideRoutes);
 app.use('/api/driver',driverRoutes);
 app.use('/api/map',mapRoutes);
 
-server.listen(port, () => {
+server.listen(port,'0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
