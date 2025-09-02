f1a1eacf37c6e1ff78832bf84c47ef6b6caa37b6
Abhishek Shukla <abhishekshukla12897@gmail.com>
2025-09-02 12:38:40 +0530

    file name change

--- frontend/src/components/waitingForDriver.jsx
+++ frontend/src/components/waitingForDriver.jsx
@@ -17,6 +17,7 @@ const WaitingForDriver= (props) => {
                 </div>
             </div>
 
+
                 <div className='w-full mt-5'>
                     <div className='flex items-center gap-5 p-3 border-b-2'>
                         <i className="ri-map-pin-user-fill"></i>
