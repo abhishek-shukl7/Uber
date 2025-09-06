a9ea40a3c824796976904f712b337d88f05a35fe
Abhishek Shukla <abhishekshukla12897@gmail.com>
2025-09-06 23:18:00 +0530

    db connection issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-function connectDB(){
+async function connectDB(){
     console.log(process.env.MONGO_URI);
-    mongoose.connect(process.env.MONGO_URI).then(() => {
+    await mongoose.connect(process.env.MONGO_URI).then(() => {
         console.log('Mongo DB Connected');
     }).catch(err => {
         console.log('MongoDB error',err);
